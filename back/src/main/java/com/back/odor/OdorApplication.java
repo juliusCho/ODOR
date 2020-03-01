996e28135ad9b65aa9d37fa15a094d6dee8a6e4a
cho.inhyo <johncrist2000@gmail.com>
2020-03-01 18:20:31 +0900

    initial git

---
+++ back/src/main/java/com/back/odor/OdorApplication.java
@@ -0,0 +1,13 @@
+package com.back.odor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdorApplication.class, args);
+    }
+
+}
