1415a52067d890ca1e1227bb57bd797f18f0e127
cho.inhyo <johncrist2000@gmail.com>
2020-04-26 20:44:46 +0900

    111

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
     <v-app-bar
-      app
-      dark
+            app
+            dark
     >
       <v-btn @click="goTo('Home')"
              style="font-size: 30px;" text>ODOR</v-btn>
@@ -10,13 +10,15 @@
       <v-spacer/>
       <v-spacer/>
       <v-spacer/>
+      <v-spacer/>
+      <v-spacer/>
 
       <MenuWithChildren
-        :menu="forum"
-        menuName="forum"
-        keyCol="forumKey"
-        labelCol="forumName"
-        @goTo="goTo"
+              :menu="forum"
+              menuName="forum"
+              keyCol="forumKey"
+              labelCol="forumName"
+              @goTo="goTo"
       />
 
       <v-spacer/>
@@ -30,16 +32,24 @@
       <v-spacer/>
 
       <MenuWithChildren
-          :menu="system"
-          menuName="system"
-          keyCol="forumKey"
-          labelCol="forumName"
-          @goTo="goTo"
+              :menu="system"
+              menuName="system"
+              keyCol="forumKey"
+              labelCol="forumName"
+              @goTo="goTo"
       />
 
       <v-spacer/>
       <v-spacer/>
       <v-spacer/>
+
+      <v-col cols="1">
+        <v-combobox
+                v-model="locale"
+                :items="localeList"
+                label="Choose your language"
+        />
+      </v-col>
 
       <v-btn  v-if="loggedInBoo"
               @click="membershipClicked"
@@ -62,9 +72,7 @@
       </v-btn>
 
     </v-app-bar>
-    // :자식객체 props 변수
-    // @:이벤트명 / 자식템플릿에서 부모템플릿에 있는 메소드 접근
-    // ref = $refs 쓸때 자식객체 지정
+
     <Router
             :routing="routing"
             @goTo="goTo"
@@ -84,42 +92,55 @@
       Router,
       MenuWithChildren
     },
-    created: function() { // == created() {
+    created() {
+      this.setSystemLocale();
       this.routing = 'Home';
       this.getForumList();
     },
-    watch: { // props, data 사용할 수 있는 객체
-      routing() { // data에서 선언한 routing을 function으로 사용
-        // rounting 데이터가 바뀌었을 때 아래 함수 실행
+    watch: {
+      routing() {
         this.$router.push(this.routing).catch(() => {});
       },
-      '$route'(to, from) { // vue 기본 객체 ($ 사인 붙은 애들), 싱글쿼테이션으로 감쌀것.
-        if (from.name === to.name) { // 비교할때
+      '$route'(to, from) {
+        if (from.name === to.name) {
           return;
         }
-        this.$refs.router.goTo(to.name); // 부모가 자식객체에 있는 메소드를 실행시키고 싶을때      loggedInBoo() {
+        this.$refs.router.goTo(to.name);
+      },
+      loggedInBoo() {
         this.sysMngrYn = this.sysMngrCheck();
         this.getSystemList();
+      },
+      locale() {
+        this.setSystemLocale();
       }
     },
     data() {
       return {
+        locale: 'ENG',
+        localeList: [
+          'ENG',
+          'KO'
+        ],
         routing: '',
         loggedInBoo: false,
         sysMngrYn: false,
         forumKey: 0,
+        categoryId: '',
         forum: [],
         system: []
       }
     },
-    props: { // 해당 선언 규칙 !
-      renee: {  // prop 에 선언된 데이터는 바꿀수 없음
-        type: Number, // 부모객체가 자식에게 값을 넣어줄 수 있음
-        default: 0,
-        required: false
-      }
-    },
     methods: {
+      // 시스템 언어 설정
+      async setSystemLocale() {
+        await axios.post(
+                API.CommonController.setSystemLocale,
+                this.locale
+        ).then(() => {
+          console.log(this.locale);
+        });
+      },
       // get system list
       async getSystemList() {
         if (!this.loggedInBoo || !this.sysMngrYn) {
@@ -130,7 +151,7 @@
         }
         await axios.post(
                 API.ForumMgmtController.getSystemList,
-                TMP_SESSION.getId()
+                TMP_SESSION.getLoginUser()
         ).then(res => {
           if (res.data.length > 0) {
             this.system = res.data;
@@ -143,21 +164,21 @@
           return;
         }
         axios.get(API.ForumMgmtController.getForumList)
-        .then(res => {
-          if (res.data.length > 0) {
-            this.forum = res.data;
-          }
-        });
+                .then(res => {
+                  if (res.data.length > 0) {
+                    this.forum = res.data;
+                  }
+                });
       },
       // routing
-      goTo(page, fKey) {
+      goTo(page, fKey, cateId) {
         if (SCRIPT_VALIDATOR.nullCheck(fKey)) {
           this.forumKey = fKey;
         }
+        if (SCRIPT_VALIDATOR.nullCheck(cateId)) {
+          this.categoryId = cateId;
+        }
         this.routing = page;
-      },
-      getForumKey() {
-        return this.forumKey;
       },
       // ************************************************************
       // Login Icon Clicked + Session check
