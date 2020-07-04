44a3b2e41779c3f51c72f2dccc9a851642885e58
cho.inhyo <johncrist2000@gmail.com>
2020-07-04 23:19:19 +0900

    just adding 1 more useless grass..

--- back/src/main/java/com/back/odor/LoggingRunner.java
+++ back/src/main/java/com/back/odor/LoggingRunner.java
@@ -11,10 +11,10 @@ public class LoggingRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-//        log.trace("완전 하찮은 서버 정보");
-//        log.debug("하찮은 서버 정보");
-//        log.info("볼만한 서버 정보");
-//        log.warn("주의사항");
-//        log.error("오류");
+        log.trace("완전 하찮은 서버 정보");
+        log.debug("하찮은 서버 정보");
+        log.info("볼만한 서버 정보");
+        log.warn("주의사항");
+        log.error("오류");
     }
 }
